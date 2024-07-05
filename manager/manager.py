d24244352f4b4cafc2d72e4cf0a9be588ac5167e
Rafaela Barbosa Trindade <rafaelabarbosatrindade@outlook.com>
2024-07-04 23:35:31 -0300

    Updating comments.

--- manager/manager.py
+++ manager/manager.py
@@ -1,77 +1,75 @@
+# Essential Libraries
 import socket
 import threading
 
-lock = threading.Lock()
-
-def thread(conn, addr):
-    # While there is a connection
+# Thread body
+def thread(conn, addr, servers):
     while True:
-
+        print()
+        print(servers)
         # Receive data
-        print(f"> Manager Status: Receiving data...")
         data = conn.recv(1024).decode("utf-8")
 
         # Loop breaker
         if not data:
-            print(f"> Manager status: No data received.")
+            print(f"\n> Manager status: No data received from {addr[0]}:{addr[1]}.")
             print(f"> Manager status: Closing connection...")
-            lock.release()
             break
 
-        print(f"> Data: {data}")
+        # Show data
+        print(f"\n> Data from {addr[0]}:{addr[1]}: {data}")
 
-        # Send data
+        # Return data
         print(f"> Manager Status: Returning data...")
         conn.sendall(data.encode("utf-8"))
         print("> Manager Status: Data sent successfully!")
-    
+
+    # Close connection
     conn.close()
     print(f"> Manager status: Connection closed.")
+    servers.remove(addr)
 
 
 def Main():
-
     # Manager info
     HOST = "127.0.0.1"  # network interface (IPv4)
     PORT = 65432        # port number
 
+    # All threads array
     threads = []
 
-    # Creates the s socket
+    # All servers address
+    servers = []
+
+    # Create a socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # Associates the socket with the manager info
+        # Associate the socket with the manager info
         s.bind((HOST, PORT))
 
         # Ennable the manager accept connections
         s.listen()
         print("> Manager Status: ON.")
-        print("> Manager Status: Waiting for connection...")
 
-        # 
         try:
             while True:
-
                 # Accept a connection
                 conn, addr = s.accept()
-                print("> Manager Status: Connection accepted.")
+                print(f"\n> Manager Status: Connected with {addr[0]}:{addr[1]}")
+                servers.append(addr)
 
-                lock.acquire()
-
-                # Connection established
-                print(f"> Manager Status: Connected with {addr[0]}:{addr[1]}")
-
-                t = threading.Thread(target=thread, args=(conn, addr))
+                # Create and start a thread
+                t = threading.Thread(target=thread, args=(conn, addr, servers))
                 t.start()
 
+                # Append thread
                 threads.append(t)
-                    
-        except KeyboardInterrupt:
-            print("Stopped by Ctrl+C.")
+        except:
+            print("\nManager closed unexpectedly.")
         finally:
             if s:
-                s.close()
+                s.close()   # Close socket
             for t in threads:
-                t.join()
+                t.join()    # Wait thread finish
 
 if __name__ == '__main__':
     Main()
--- server/server.py
+++ server/server.py
@@ -12,21 +12,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("> Server Status: Connection accepted.")
 
     while True:
-        
-        # Input text data
-        msg = input("> Write a message: ")
+        # Receive data from input
+        msg = input("\n> Write a message: ")
 
+        # Loop breaker
         if msg == "":
-            print("> Server Status: No data sent.")
+            print("\n> Server Status: No data sent.")
             print("> Server Status: Exiting server...")
             break
 
-        # Sends data to Manager
-        print("> Server Status: Sending data...")
+        # Send data to Manager
+        print("\n> Server Status: Sending data...")
         s.sendall(msg.encode("utf-8"))
         print("> Server Status: Data sent successfully!")
 
-        # Receives data from Manager
-        print("> Server Status: Receiving data...")
+        # Receive data from Manager
+        print("\n> Server Status: Receiving data...")
         data = s.recv(1024).decode("utf-8")
         print(f"> Data: {data}")
